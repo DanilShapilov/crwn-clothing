ac2ed3b9ddb68fcd8665c71dc67d6e310a988920
DanilShapilov <danil.shapilov225@gmail.com>
2021-08-28 13:15:00 +0500

    updation app with a shop page, collection preview component and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,10 @@
-import Directory from '../../components/directory/directory.component'
-import './homepage.styles.scss'
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
 
 const HomePage = () => (
-  <div className='homepage'>
+  <div className="homepage">
     <Directory />
   </div>
-)
+);
 
-export default HomePage
+export default HomePage;
