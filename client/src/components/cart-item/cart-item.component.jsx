e0a3c5b4bbda677580f18c7d7c202e2201c2bdfe
Danil <danil@shapilov.dev>
2021-10-03 12:52:09 +0500

    adding performance optimizations to codebase with react lazy, suspense and memo

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   CartItemContainer,
   CartItemImage,
@@ -16,4 +17,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   </CartItemContainer>
 );
 
-export default CartItem;
+export default React.memo(CartItem);
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route, useRouteMatch } from "react-router-dom";
-import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 
 import { useDispatch } from "react-redux";
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+import Spinner from "../../components/spinner/spinner.component";
 
-import CollectionPageContainer from "../collection/collection.container";
+const CollectionsOverviewContainer = lazy(() =>
+  import("../../components/collections-overview/collections-overview.container")
+);
+const CollectionPageContainer = lazy(() =>
+  import("../collection/collection.container")
+);
 
 const ShopPage = () => {
   const { path } = useRouteMatch();
@@ -16,11 +21,17 @@ const ShopPage = () => {
 
   return (
     <div className="shop-page">
-      <Route exact path={`${path}`} component={CollectionsOverviewContainer} />
-      <Route
-        path={`${path}/:collectionId`}
-        component={CollectionPageContainer}
-      />
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
     </div>
   );
 };
